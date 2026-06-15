eaa2c1a335b7b3a3876a3511adb74f9b90111365
root <root@Telenity-PC1.localdomain>
2026-06-15 13:58:58 +0300

    Add Google Analytics GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import CountryPopup from "../components/CountryPopup";
+import GoogleAnalytics from "../components/analytics/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,6 +41,7 @@ export default function RootLayout({
       >
 	<>
 	  <CountryPopup />
+	  <GoogleAnalytics />
 	  {children}
 	</>
       </body>
