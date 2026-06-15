2ade187337023cb40d09678100d0ea43103fcd6c
root <root@Telenity-PC1.localdomain>
2026-06-15 18:55:22 +0300

    Add robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://hub4deals.com/sitemap.xml",
+  };
+}
