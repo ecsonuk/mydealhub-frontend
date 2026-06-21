d877a9e429aea12cb5646f480efecce350706c3d
root <root@Telenity-PC1.localdomain>
2026-06-21 22:15:55 +0300

    Impact Verification TextID Placement-2

--- app/impact-verification/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ImpactVerification() {
-  return (
-    <div>
-      Impact-Site-Verification: 1d2583ee-db97-4f72-a624-6e251f4e4846
-    </div>
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -147,7 +147,12 @@ import {
 
       </main>
 
+	<div style={{ display: "none" }}>
+	  Impact-Site-Verification: 1d2583ee-db97-4f72-a624-6e251f4e4846
+	</div>
+
       <Footer />
+
     </>
   );
 }
